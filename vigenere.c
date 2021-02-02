cc5378c6ff2d4dde8c9fc6a85b04403d145bd3f5
GuillaumeCothenet <cothenet.guillaume@gmail.com>
2021-02-02 08:04:00 +0100

    Premier commit

--- vigenere.c
+++ vigenere.c
@@ -8,3 +8,20 @@ int main(int argc, char *argv[])
 	printf("%s\n", texte);
 	return 0;
 }
+
+char convertion(char letterToConvert, char keyLetter){
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int keyNum, letterNum;
+	for(int i = 0; i <= 26; i++){
+		if(alphabet[i] == keyLetter){
+			keyNum = i;
+		}
+	}
+	for(int i = 0; i <= 26; i++){
+		if(alphabet[i] == letterToConvert){
+			letterNum = i;
+		}
+	}
+	
+	return alphabet[(letterNum + keyNum)%26];
+}
